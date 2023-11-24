5d4f14a8efb7aad488d331cc804aef811cc60db4
rofinafiin <rofinafiisr@gmail.com>
2023-11-24 14:34:13 +0700

    fix(sidebar): release html di package javascript

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,5 @@
 $(function () {
   // Load navbar.html on page load
-  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/pmb-mhs@0.0.1/static/sidebar.html");
+  $("#sidebar-container").load("./static/sidebar.html");
 
 });
