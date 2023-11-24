c92ec76f44cee56a1697a7a32f27416210724652
rofinafiin <rofinafiisr@gmail.com>
2023-11-24 14:51:20 +0700

    fix(sidebar): fix import

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,5 @@
 $(function () {
   // Load navbar.html on page load
-  $("#sidebar-container").load("./static/sidebar.html");
+  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.0.5/static/sidebar.html");
 
 });
