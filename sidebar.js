f184fde4d026d2f7608efa8eb141e9f7adb0267e
rofinafiin <rofinafiisr@gmail.com>
2023-11-24 16:24:34 +0700

    fixing miss version

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,5 @@
 $(function () {
   // Load navbar.html on page load
-  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.0.10/static/sidebar.html");
+  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.1.1/static/sidebar.html");
 
 });
