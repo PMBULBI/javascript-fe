fb45518fea26b140bee00ec70963334a07fff994
rofinafiin <rofinafiisr@gmail.com>
2023-11-27 04:29:51 +0000

    fix(header) : fit header dinamis

--- bar/sidebar.js
+++ bar/sidebar.js
@@ -1,5 +1,5 @@
 $(function () {
   // Load navbar.html on page load
-  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.1.1/static/sidebar.html");
+  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.1.3/static/sidebar.html");
 
 });
