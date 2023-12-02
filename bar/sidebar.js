c0093516268225a2fbd3bb60f04b7d632fba2049
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-02 18:18:36 +0700

    sidebar 0.2.2

--- bar/sidebar.js
+++ bar/sidebar.js
@@ -1,5 +1,5 @@
 $(function () {
   // Load navbar.html on page load
-  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.2.1/static/sidebar.html");
+  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.2.2/static/sidebar.html");
 
 });
